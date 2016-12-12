fd71beccf04aee052cdf85ee6ffaf0744c9cfee2
ragsweb <ragsweb@outlook.com>
2016-12-11 22:31:08 -0200

    Initial

---
+++ LanguageServicesProtocol/LanguageServer/SetPSSARulesRequest.cs
@@ -0,0 +1,16 @@
+﻿//
+// Copyright (c) Microsoft. All rights reserved.
+// Licensed under the MIT license. See LICENSE file in the project root for full license information.
+//
+
+using Microsoft.LanguageServer.EditorServices.Protocol.MessageProtocol;
+
+namespace Microsoft.LanguageServer.EditorServices.Protocol.LanguageServer
+{
+    class SetPSSARulesRequest
+    {
+        public static readonly
+            RequestType<object, object> Type =
+            RequestType<object, object>.Create("powerShell/setPSSARules");
+    }
+}
---
+++ README.md
@@ -0,0 +1,3 @@
+# advpl-language-server README
+
+Implementação do protocolo Language Service em C# para para a linguagem ADVPL.
